43c0102465384c8d71170acbb82687ca01d4d896
Carolina Marianela <gallegos.carolina@outlook.com>
2022-08-25 16:41:38 +0100

    Agregado de comentarios

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :articles do
     resources :comments
+    resources :posts
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
