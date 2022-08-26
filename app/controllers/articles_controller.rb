a5c3320d9a6423cba5d2175b150e2c6328b6582f
Carolina Marianela <gallegos.carolina@outlook.com>
2022-08-26 10:00:56 +0100

    remove coments+password

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "carol", password: "secret", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
